c27cc44e4b96625d948d7cc5c774c2998f93a5a0
pascal garcia <pgarcia@cxignited.com>
2019-01-08 00:28:25 +0100

    add endpoint : get comments by genre

--- backend/src/main/java/com/bonitasoft/reactiveworkshop/api/GenreApi.java
+++ backend/src/main/java/com/bonitasoft/reactiveworkshop/api/GenreApi.java
@@ -3,33 +3,43 @@ package com.bonitasoft.reactiveworkshop.api;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.bonitasoft.reactiveworkshop.domain.Artist;
-import com.bonitasoft.reactiveworkshop.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import com.bonitasoft.reactiveworkshop.domain.Artist;
+import com.bonitasoft.reactiveworkshop.domain.Comment;
+import com.bonitasoft.reactiveworkshop.repository.ArtistRepository;
+import com.bonitasoft.reactiveworkshop.service.web.CommentsService;
 
 @RestController
 public class GenreApi {
 
+	private ArtistRepository artistRepository;
+	private CommentsService commentsRepository;
 
+	@Autowired
+	public GenreApi(ArtistRepository artistRepository, CommentsService commentsRepository) {
+		this.artistRepository = artistRepository;
+		this.commentsRepository = commentsRepository;
+	}
 
-    private ArtistRepository artistRepository;
-    private RestTemplate restTemplate;
+	@GetMapping("/genres")
+	public List<String> findAll() {
+		return artistRepository.findAll().stream().map(Artist::getGenre).filter(g -> !g.isEmpty()).distinct().sorted()
+				.collect(Collectors.toList());
+	}
 
-    public GenreApi(ArtistRepository artistRepository, RestTemplate restTemplate) {
-        this.artistRepository = artistRepository;
-        this.restTemplate = restTemplate;
-    }
-
-    @GetMapping("/genres")
-    public List<String> findAll() {
-        return artistRepository.findAll().stream()
-                .map(Artist::getGenre)
-                .filter(g -> !g.isEmpty())
-                .distinct()
-                .sorted()
-                .collect(Collectors.toList());
-    }
+	@GetMapping("/genres/{genre}/comments")
+	public List<Comment> findCommentsByGenre(@PathVariable String genre) {
+		return artistRepository.findByGenre(genre).stream().map(artist -> {
+					return commentsRepository.getCommentsByArtisteId(artist.getId()).stream().map(comment -> {
+						comment.setArtisteId(artist.getId());
+						comment.setArtistName(artist.getName());
+						return comment;
+					}).collect(Collectors.toList());
+				}).flatMap(List::stream).collect(Collectors.toList());
+	}
 
 }
