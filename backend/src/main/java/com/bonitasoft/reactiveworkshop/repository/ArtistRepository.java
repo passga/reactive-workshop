06ec5ea4c7a785e237f9b0b8647164732664999e
pascal garcia <pgarcia@cxignited.com>
2019-01-15 05:41:11 +0100

    change repositiory to using mongo

--- backend/src/main/java/com/bonitasoft/reactiveworkshop/repository/ArtistRepository.java
+++ backend/src/main/java/com/bonitasoft/reactiveworkshop/repository/ArtistRepository.java
@@ -3,16 +3,14 @@ package com.bonitasoft.reactiveworkshop.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bonitasoft.reactiveworkshop.domain.Artist;
 
 @Repository
-public interface ArtistRepository extends JpaRepository<Artist, String> {// extends MongoRepository<Artist, String> {
+public interface ArtistRepository extends MongoRepository<Artist, String> {
 
-	@Query("SELECT art FROM Artist art  WHERE art.genre=(:genre)")
 	Optional<List<Artist>> findByGenre(@Param("genre") String genre);
 }
